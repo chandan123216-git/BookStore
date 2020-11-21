827d15948238e405d2bbbfff2f5523055e5052b4
Chandan <patil.chandan123216@gmail.com>
2020-11-21 17:06:58 +0530

    Add Model and Repository

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebGentle.BookStore.Models;
+using WebGentle.BookStore.Repositary;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(String Title, String Author)
+        {
+            return _bookRepository.SearchBook(Title, Author);
+        }
+    }
+}
---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class HomeController 
+    {
+        public string Index()
+        {
+            return "Web Gentle Book Store !";
+        }
+    }
+}
